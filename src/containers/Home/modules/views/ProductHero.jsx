4ab69d8366e13ca1d4066bdb0e6710e84d58a7e0
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-12 18:35:08 -0500

    Starting toolbar, product hero, product hero layout from homepage

---
+++ src/containers/Home/modules/views/ProductHero.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+const backgroundImage =
+  'https://cdn.pixabay.com/photo/2017/02/03/03/54/burger-2034433_960_720.jpg';
+
+const styles = (theme) => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Vive la mejor experiencia en Carnes Angus
+      </Typography>
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        En BurgerTown contamos con la mejor carne de res tipo Angus certificada
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+      >
+        Ir al Menú
+      </Button>
+      <Typography variant="body2" color="inherit" className={classes.more}>
+        También somos expertos en Perros y Salchipapas. Descubre la auténtica experiencia de comida rápida americana con nosotros!
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
